d471d546f6fa1a692364da1734d7d0026ce19882
kim-yongmin <kim-yongmin@localhost>
2023-03-31 00:56:54 +0900

    Update BOJ

---
+++ BaekJoon/src/기본수학1/Q2745.java
@@ -0,0 +1,26 @@
+package 기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2745 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		String b = st.nextToken();
+		int n = Integer.parseInt(st.nextToken())-1;
+
+		int result = 0;
+		for(int i = 0; i < b.length(); i++) {
+			char ch = b.charAt(i);
+			int number = 'A' <= ch && ch <= 'Z' ? ch - '7' : ch - '0';
+			result += result*n + number;
+		}
+		System.out.println(result);
+	}
+}
+
